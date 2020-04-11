5e2152875997bc2c6620f0f97e7f893432cbbc62
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-04-11 20:14:46 +0300

    step1-2

--- my-list/src/index.js
+++ my-list/src/index.js
@@ -2,7 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+//STEP 1 Design the state needed for the app
+const initialState={
+  formValues:{
+   todoList:[],
+   task:"",
+  },
+  todos: [
+    {
+      id:3829875789,
+      item:'Learn about reducers',
+      completed:false
+    },
+    {
+      id:234234234,
+      item:'Learn about redux',
+      completed:false
+    },
+  ]
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +30,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
---
+++ my-list/src/state/actionType.js
@@ -0,0 +1,5 @@
+//STEP 2: Design the type of action the state may suffer
+export const INPUT_CHANGE="INPUT_CHANGE"
+export const ADD_TODO ="ADD_TODO"
+export const TOGGLE_TODO="TOGGLE_TODO"
+export const CLEAR_COMPLETED="CLEAR_COMPLETED"
