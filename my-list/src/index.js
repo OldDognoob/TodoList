096e91e0a401dff87d9fa8a5227fd9a90c848b7c
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-04-11 21:12:36 +0300

    complete index

--- my-list/src/index.js
+++ my-list/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {combineReducers,createStore} from 'redux';
+import {Provider} from 'react-redux'
+import {formReducer, todoReducer} from './state/reducer';
 import './index.css';
 import App from './App';
 
@@ -23,10 +26,23 @@ const initialState={
   ]
 }
 
+//STEP 4 Use combine reducers to make a monster reducer
+const combineReducer = combineReducers({
+  formValues:formReducer,
+  todos: todoReducer,
+})
+//STEP 5 Use create store to make a redux store
+const store = createStore(
+  combineReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+
 ReactDOM.render(
-  <React.StrictMode>
+  // STEP 6 Wrap the application with a provider from react-redux
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
