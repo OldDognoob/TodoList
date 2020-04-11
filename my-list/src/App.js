3aff5ecb5f23a7a53baaf9928fb6d5a246ad993c
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-04-11 21:49:06 +0300

    complete app

--- my-list/src/App.js
+++ my-list/src/App.js
@@ -1,26 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
+import { connect } from 'react-redux';
+import {addTodo,toggleTodo,inputChange,clearCompleted} from './state/actionCreator'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App({
+  // A data from redux state
+  formValues,
+  todos,
+  //B callbacks to change state(action creators)
+  inputChange,
+  toggleTodo,
+  addTodo,
+  clearCompleted,
+  //C props injected by the parent component
+}) {
+  //Event Listeners
+const onSubmit=event=>{
+  event.preventDefault()
+  addTodo({
+    task: formValues.task
+  })
+}
+const onChange=event=>{
+  inputChange({
+    task:event.target.value
+  })
+
+
+}
+const onAddTodo =event => {
+  addTodo(formValues)
+}
+const onClearCompleted = e => {
+  clearCompleted()
+}
+const onToggleTodo = id => event =>{
+  toggleTodo(id)
+}
+return(
+  <div className="App">
+    <h1>Add a new todo</h1>
+    <form>
+      <input onChange={onChange} name='task' value={formValues.task}></input>
+      
+      {console.log(formValues)}
+      <button type='button' onClick={onSubmit}>Add</button>
+      <button type='button' onClick={onClearCompleted}>Clear</button>
+    </form>
+    {todos.map(todo => <p onClick={onToggleTodo(todo.id)}>{todo.task}</p>)}
+  </div>
+)
 }
 
-export default App;
+//STEP-8 USE connect FROM REACT-REDUX TO WRAP OUR COMPONENT
+function mapStateToProps(state){
+  return{
+    formValues: state.formValues,
+    todos:state.todos
+  };
+}
+export default connect(mapStateToProps, {
+  inputChange,
+  addTodo,
+  toggleTodo,
+  clearCompleted
+})(App);
